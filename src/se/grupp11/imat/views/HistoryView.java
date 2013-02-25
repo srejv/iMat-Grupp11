2c415f9dd2003fab155f3aa8acb0bb6d39e8d5a5
srejv <oscar.dragen@gmail.com>
2013-02-25 15:01:55 +0100

    History View

---
+++ src/se/grupp11/imat/views/HistoryView.java
@@ -0,0 +1,18 @@
+package se.grupp11.imat.views;
+
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+
+public class HistoryView extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public HistoryView() {
+		
+		JLabel lblHistory = new JLabel("History");
+		add(lblHistory);
+
+	}
+
+}
