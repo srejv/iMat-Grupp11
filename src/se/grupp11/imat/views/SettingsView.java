bd75d0e3d39be2ceab7544a963ee1e1874e87db3
srejv <oscar.dragen@gmail.com>
2013-02-25 14:47:40 +0100

    SettingsView updated variable names

--- src/se/grupp11/imat/views/SettingsView.java
+++ src/se/grupp11/imat/views/SettingsView.java
@@ -11,14 +11,14 @@ import javax.swing.JButton;
 import java.awt.Font;
 
 public class SettingsView extends JPanel {
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
-	private JTextField textField_3;
-	private JTextField textField_4;
-	private JTextField textField_5;
-	private JTextField textField_6;
-	private JTextField textField_7;
+	private JTextField textFieldFirstName;
+	private JTextField textFieldLastName;
+	private JTextField textFieldAdress;
+	private JTextField textFieldPostalCode;
+	private JTextField textFieldCity;
+	private JTextField textFieldEmail;
+	private JTextField textFieldCellphone;
+	private JTextField textFieldCardNumber;
 
 	/**
 	 * Create the panel.
@@ -61,65 +61,65 @@ public class SettingsView extends JPanel {
 				FormFactory.RELATED_GAP_ROWSPEC,
 				FormFactory.DEFAULT_ROWSPEC,}));
 		
-		JLabel lblNewLabel = new JLabel("Settings");
-		lblNewLabel.setFont(new Font("Lucida Grande", Font.BOLD, 24));
-		add(lblNewLabel, "1, 2, 1, 2, left, top");
+		JLabel lblSettingsTitle = new JLabel("Settings");
+		lblSettingsTitle.setFont(new Font("Lucida Grande", Font.BOLD, 24));
+		add(lblSettingsTitle, "1, 2, 1, 2, left, top");
 		
-		JLabel lblNewLabel_1 = new JLabel("First name");
-		add(lblNewLabel_1, "1, 6, right, default");
+		JLabel lblFirstName = new JLabel("First name");
+		add(lblFirstName, "1, 6, right, default");
 		
-		textField = new JTextField();
-		add(textField, "2, 6, fill, default");
-		textField.setColumns(10);
+		textFieldFirstName = new JTextField();
+		add(textFieldFirstName, "2, 6, fill, default");
+		textFieldFirstName.setColumns(10);
 		
 		JLabel lblLastName = new JLabel("Last name");
 		add(lblLastName, "1, 8, right, default");
 		
-		textField_1 = new JTextField();
-		add(textField_1, "2, 8, fill, default");
-		textField_1.setColumns(10);
+		textFieldLastName = new JTextField();
+		add(textFieldLastName, "2, 8, fill, default");
+		textFieldLastName.setColumns(10);
 		
-		JLabel lblNewLabel_2 = new JLabel("Adress");
-		add(lblNewLabel_2, "1, 10, right, default");
+		JLabel lblAdress = new JLabel("Adress");
+		add(lblAdress, "1, 10, right, default");
 		
-		textField_2 = new JTextField();
-		add(textField_2, "2, 10, fill, default");
-		textField_2.setColumns(10);
+		textFieldAdress = new JTextField();
+		add(textFieldAdress, "2, 10, fill, default");
+		textFieldAdress.setColumns(10);
 		
-		JLabel lblNewLabel_3 = new JLabel("Postal Code");
-		add(lblNewLabel_3, "1, 12, right, default");
+		JLabel lblPostalCode = new JLabel("Postal Code");
+		add(lblPostalCode, "1, 12, right, default");
 		
-		textField_3 = new JTextField();
-		add(textField_3, "2, 12, fill, default");
-		textField_3.setColumns(10);
+		textFieldPostalCode = new JTextField();
+		add(textFieldPostalCode, "2, 12, fill, default");
+		textFieldPostalCode.setColumns(10);
 		
 		JLabel lblCity = new JLabel("City");
 		add(lblCity, "1, 14, right, default");
 		
-		textField_4 = new JTextField();
-		add(textField_4, "2, 14, fill, default");
-		textField_4.setColumns(10);
+		textFieldCity = new JTextField();
+		add(textFieldCity, "2, 14, fill, default");
+		textFieldCity.setColumns(10);
 		
-		JLabel lblNewLabel_4 = new JLabel("Email");
-		add(lblNewLabel_4, "1, 18, right, default");
+		JLabel lblEmail = new JLabel("Email");
+		add(lblEmail, "1, 18, right, default");
 		
-		textField_5 = new JTextField();
-		add(textField_5, "2, 18, fill, default");
-		textField_5.setColumns(10);
+		textFieldEmail = new JTextField();
+		add(textFieldEmail, "2, 18, fill, default");
+		textFieldEmail.setColumns(10);
 		
 		JLabel lblCellphone = new JLabel("Cellphone");
 		add(lblCellphone, "1, 20, right, default");
 		
-		textField_6 = new JTextField();
-		add(textField_6, "2, 20, fill, default");
-		textField_6.setColumns(10);
+		textFieldCellphone = new JTextField();
+		add(textFieldCellphone, "2, 20, fill, default");
+		textFieldCellphone.setColumns(10);
 		
 		JLabel lblCardNumber = new JLabel("Card number");
 		add(lblCardNumber, "1, 24, right, default");
 		
-		textField_7 = new JTextField();
-		add(textField_7, "2, 24, fill, default");
-		textField_7.setColumns(10);
+		textFieldCardNumber = new JTextField();
+		add(textFieldCardNumber, "2, 24, fill, default");
+		textFieldCardNumber.setColumns(10);
 		
 		JButton btnSave = new JButton("Save");
 		add(btnSave, "2, 28");
