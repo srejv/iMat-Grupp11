46e9e73ca4ac8f1467b09f76acad1ec5cd749785
Oscar <oscar.dragen@gmail.com>
2013-02-25 17:15:46 +0100

    fixat lite med deps

--- src/se/grupp11/imat/views/ProductList.java
+++ src/se/grupp11/imat/views/ProductList.java
@@ -1,6 +1,11 @@
 package se.grupp11.imat.views;
 
 import javax.swing.JPanel;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+import net.miginfocom.swing.MigLayout;
+import java.awt.BorderLayout;
 
 public class ProductList extends JPanel {
 
@@ -8,6 +13,7 @@ public class ProductList extends JPanel {
 	 * Create the panel.
 	 */
 	public ProductList() {
+		setLayout(new BorderLayout(0, 0));
 
 	}
 
