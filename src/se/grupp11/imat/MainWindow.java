618d26e82d086d7a16db3ccc3d6a7c692685b5bf
srejv <oscar.dragen@gmail.com>
2013-02-25 14:33:31 +0100

    Added Menu Items
    La till menu items från Prototypbilderna

--- src/se/grupp11/imat/MainWindow.java
+++ src/se/grupp11/imat/MainWindow.java
@@ -9,6 +9,8 @@ import javax.swing.JPanel;
 import java.awt.BorderLayout;
 import javax.swing.JTextField;
 import javax.swing.JButton;
+import javax.swing.JMenuItem;
+import javax.swing.JSeparator;
 
 public class MainWindow {
 
@@ -36,14 +38,77 @@ public class MainWindow {
 		JMenu mnFile = new JMenu("File");
 		menuBar.add(mnFile);
 		
+		JMenuItem mntmNewList = new JMenuItem("New List");
+		mnFile.add(mntmNewList);
+		
+		JMenuItem mntmToCheckout = new JMenuItem("To Checkout");
+		mnFile.add(mntmToCheckout);
+		
+		JMenuItem mntmEmptyShoppingCart = new JMenuItem("Empty Shopping Cart");
+		mnFile.add(mntmEmptyShoppingCart);
+		
+		JSeparator separator = new JSeparator();
+		mnFile.add(separator);
+		
+		JMenuItem mntmExit = new JMenuItem("Exit");
+		mnFile.add(mntmExit);
+		
 		JMenu mnEdit = new JMenu("Edit");
 		menuBar.add(mnEdit);
+		
+		JMenuItem mntmUndo = new JMenuItem("Undo");
+		mnEdit.add(mntmUndo);
+		
+		JMenuItem mntmRedo = new JMenuItem("Redo");
+		mnEdit.add(mntmRedo);
+		
+		JSeparator separator_1 = new JSeparator();
+		mnEdit.add(separator_1);
+		
+		JMenuItem mntmCut = new JMenuItem("Cut");
+		mnEdit.add(mntmCut);
+		
+		JMenuItem mntmCopy = new JMenuItem("Copy");
+		mnEdit.add(mntmCopy);
+		
+		JMenuItem mntmPaste = new JMenuItem("Paste");
+		mnEdit.add(mntmPaste);
+		
+		JMenuItem mntmDelete = new JMenuItem("Delete");
+		mnEdit.add(mntmDelete);
+		
+		JSeparator separator_2 = new JSeparator();
+		mnEdit.add(separator_2);
+		
+		JMenuItem mntmSelectAll = new JMenuItem("Select All");
+		mnEdit.add(mntmSelectAll);
+		
+		JMenuItem mntmDeselectAll = new JMenuItem("Deselect All");
+		mnEdit.add(mntmDeselectAll);
 		
 		JMenu mnView = new JMenu("View");
 		menuBar.add(mnView);
 		
+		JMenuItem mntmMinimize = new JMenuItem("Minimize");
+		mnView.add(mntmMinimize);
+		
+		JMenuItem mntmMaximize = new JMenuItem("Maximize");
+		mnView.add(mntmMaximize);
+		
+		JSeparator separator_3 = new JSeparator();
+		mnView.add(separator_3);
+		
+		JMenuItem mntmForward = new JMenuItem("Forward");
+		mnView.add(mntmForward);
+		
+		JMenuItem mntmBack = new JMenuItem("Back");
+		mnView.add(mntmBack);
+		
 		JMenu mnHelp = new JMenu("Help");
 		menuBar.add(mnHelp);
+		
+		JMenuItem mntmAbout = new JMenuItem("About...");
+		mnHelp.add(mntmAbout);
 		
 		JPanel panel = new JPanel();
 		frame.getContentPane().add(panel, BorderLayout.NORTH);
