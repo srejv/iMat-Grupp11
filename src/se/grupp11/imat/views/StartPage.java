e63437414cfe42106ccdaf64c136c846b0e2924d
Hampus Forsvall <forzvall@gmail.com>
2013-02-27 14:14:15 +0100

    Made Startpage scrollable

--- src/se/grupp11/imat/views/StartPage.java
+++ src/se/grupp11/imat/views/StartPage.java
@@ -4,6 +4,7 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 
 import net.miginfocom.swing.MigLayout;
+import java.awt.GridLayout;
 
 public class StartPage extends JPanel {
 	private final ProductSquareItem productSquareItem_4 = new ProductSquareItem();
@@ -12,54 +13,54 @@ public class StartPage extends JPanel {
 	 * Create the panel.
 	 */
 	public StartPage() {
-		setLayout(new MigLayout("", "[190px,grow][190px,grow][190px,grow][190px,grow]", "[561px][grow][grow][grow]"));
+		setLayout(new GridLayout(4, 4, 0, 0));
 		
 		
 		ProductSquareItem productSquareItem = new ProductSquareItem();
-		add(productSquareItem, "cell 0 0,grow");
+		add(productSquareItem);
 		
 		ProductSquareItem productSquareItem_1 = new ProductSquareItem();
-		add(productSquareItem_1, "cell 1 0,grow");
+		add(productSquareItem_1);
 		
 		ProductSquareItem productSquareItem_2 = new ProductSquareItem();
-		add(productSquareItem_2, "cell 2 0,grow");
+		add(productSquareItem_2);
 		
 		ProductSquareItem productSquareItem_3 = new ProductSquareItem();
-		add(productSquareItem_3, "cell 3 0,grow");
+		add(productSquareItem_3);
 		
 		ProductSquareItem productSquareItem_5 = new ProductSquareItem();
-		add(productSquareItem_5, "cell 0 1,grow");
-		add(productSquareItem_4, "cell 1 1");
+		add(productSquareItem_5);
+		add(productSquareItem_4);
 		
 		ProductSquareItem productSquareItem_9 = new ProductSquareItem();
-		add(productSquareItem_9, "cell 2 1,grow");
+		add(productSquareItem_9);
 		
 		ProductSquareItem productSquareItem_13 = new ProductSquareItem();
-		add(productSquareItem_13, "cell 3 1,grow");
+		add(productSquareItem_13);
 		
 		ProductSquareItem productSquareItem_6 = new ProductSquareItem();
-		add(productSquareItem_6, "cell 0 2,grow");
+		add(productSquareItem_6);
 		
 		ProductSquareItem productSquareItem_8 = new ProductSquareItem();
-		add(productSquareItem_8, "cell 1 2,grow");
+		add(productSquareItem_8);
 		
 		ProductSquareItem productSquareItem_10 = new ProductSquareItem();
-		add(productSquareItem_10, "cell 2 2,grow");
+		add(productSquareItem_10);
 		
 		ProductSquareItem productSquareItem_14 = new ProductSquareItem();
-		add(productSquareItem_14, "cell 3 2,grow");
+		add(productSquareItem_14);
 		
 		ProductSquareItem productSquareItem_7 = new ProductSquareItem();
-		add(productSquareItem_7, "cell 0 3,grow");
+		add(productSquareItem_7);
 		
 		ProductSquareItem productSquareItem_11 = new ProductSquareItem();
-		add(productSquareItem_11, "cell 1 3,grow");
+		add(productSquareItem_11);
 		
 		ProductSquareItem productSquareItem_12 = new ProductSquareItem();
-		add(productSquareItem_12, "cell 2 3,grow");
+		add(productSquareItem_12);
 		
 		ProductSquareItem productSquareItem_15 = new ProductSquareItem();
-		add(productSquareItem_15, "cell 3 3,grow");
+		add(productSquareItem_15);
 
 	}
 }
