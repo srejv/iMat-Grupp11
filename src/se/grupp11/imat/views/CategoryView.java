3c25b4e3646d0ccaf3b722d6beb9ee6345fd5faf
Markus <Markus.a.noren@gmail.com>
2013-03-06 15:36:11 +0100

    grids

--- src/se/grupp11/imat/views/CategoryView.java
+++ src/se/grupp11/imat/views/CategoryView.java
@@ -21,6 +21,7 @@ import java.awt.BorderLayout;
 import javax.swing.JScrollPane;
 import javax.swing.ScrollPaneConstants;
 import java.awt.GridLayout;
+import java.awt.FlowLayout;
 
 public class CategoryView extends JPanel  {
 	
@@ -39,9 +40,8 @@ public class CategoryView extends JPanel  {
 		add(lblCategory, BorderLayout.NORTH);
 		
 		productGrid = new JPanel();
-		add(productGrid, BorderLayout.SOUTH);
+		add(productGrid);
 		productGrid.setBackground(new Color(255, 250, 250));
-		productGrid.setLayout(new GridLayout(4, 4, 0, 0));
 		
 
 	}
@@ -51,13 +51,21 @@ public class CategoryView extends JPanel  {
 		
 		productGrid.removeAll();
 		List<Product> prodList = IMatDataHandler.getInstance().getProducts(category);
-		//int rows = prodList.size() / 4;
-		productGrid.setLayout(new GridLayout(4,4,0,0));
+		int rows = (prodList.size() / 4)+1;
+		System.out.println(prodList.size() + "");
+		System.out.println(rows + "");
 		
+		productGrid.setLayout(new GridLayout(4,4));
+		
+		int nulllbls=16-prodList.size();
 		for (Product product : prodList) {
 			ProductSquareItem i = new ProductSquareItem(product, 1);
 			productGrid.add(i);
 		}
+		for(int i=0; i<nulllbls; i++){
+			productGrid.add(new JLabel(""));
+		}
+
 		
 		this.updateUI();
 	}
